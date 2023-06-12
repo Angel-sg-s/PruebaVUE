9c54f2b5ac1a12a0faa0d18d00e6cf3c0b22e7ff
Angel-sg-s <114315557+Angel-sg-s@users.noreply.github.com>
2023-06-12 17:40:07 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+<template>
+  <div class=" fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
+    <ul  class="flex items-center"
+        v-for="item in items"
+        :key="item.id">
+      <RouterLink :to="item.name"
+       
+        @click="addTab(item)"
+      >
+        {{ item.name }}
+      </RouterLink>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, name: "Página_1" },
+        { id: 2, name: "Página_2" },
+        { id: 3, name: "Página_3" },
+      ],
+    };
+  },
+  methods: {
+    addTab(item) {
+      this.$emit("addTab", item);
+    },
+  },
+};
+</script>
